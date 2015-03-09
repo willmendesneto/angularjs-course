5f52fb0bb9a4f53c8285853eaf128cf6015d93c0
willmendesneto <willmendesneto@gmail.com>
2015-03-09 00:18:00 -0300

    feat: add step-5

--- app/scripts/controllers/contacts.js
+++ app/scripts/controllers/contacts.js
@@ -8,7 +8,7 @@
 (function() {
   'use strict';
 
-  function ContactsCtrl($scope, $location, ContactsService) {
+  function ContactsCtrl($scope, $location, $window, $routeParams, $route, $filter, ContactsService) {
 
     this.contact = [];
 
@@ -24,12 +24,43 @@
 
     this.create = function(contact){
       this.listContacts = ContactsService.create(contact);
+    };
+
+    this.edit = function(){
+      var id = $routeParams.id;
+      this.contact = $filter('filter')(this.listContacts, {_id: id})[0];
+      if (this.contactExists()) {
+        $window.scrollTo(0, 0);
+      } else {
+        $location.path('/contacts/new');
+      }
+    };
+
+    this.contactExists = function(){
+      return typeof this.contact !== 'undefined';
+    };
+
+    this.update = function( item ) {
+      this.listContacts = ContactsService.update(item);
+    };
+
+    this.save = function(item){
+      if(typeof item._id !== 'undefined'){
+        this.update(item);
+      } else {
+        this.create(item);
+      }
       this.reset();
       $location.path('/contacts');
     };
 
     this.init = function(){
       this.listContacts = this.filteredData = ContactsService.getListItems();
+      this.reset();
+      //  Calling routeParam method
+      if ($route.current.method !== undefined) {
+        this[$route.current.method]();
+      }
     };
 
     this.init();
@@ -39,6 +70,6 @@
   angular.module('angularjsCourseApp')
     .controller('ContactsCtrl', ContactsCtrl);
 
-  ContactsCtrl.$inject = ['$scope', '$location', 'ContactsService'];
+  ContactsCtrl.$inject = ['$scope', '$location', '$window', '$routeParams', '$route', '$filter', 'ContactsService'];
 
 }());
